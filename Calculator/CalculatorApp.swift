0832896333239a4027055818fd9c0035abb54d19
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-12 17:19:29 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Logan Snopek on 2021-12-12.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
