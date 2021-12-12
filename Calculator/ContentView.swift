f0d57d4cf6d11c0af79519fa765aaba68a4a20b0
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-12 17:46:50 -0500

    Done set-up got slider linked to variables

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored proprotys
+    
+    @State var lenght: Double = 50
+    @State var hight: Double = 50
+    @State var with: Double = 50
+    
+    //MARK: Computed proprotes
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            
+        VStack{
+        
+        Slider(value: $lenght,
+               in: 0.0...100.0,
+               label: {
+                    Text("Opacity")
+        },
+               minimumValueLabel: {
+                    Text("0")
+        },
+               maximumValueLabel: {
+                    Text("100")
+        })
+        
+        
+        Slider(value: $hight,
+               in: 0.0...100.0,
+               label: {
+                    Text("Opacity")
+        },
+               minimumValueLabel: {
+                    Text("0")
+        },
+               maximumValueLabel: {
+                    Text("100")
+        })
+        
+        
+        
+        Slider(value: $with,
+               in: 0.0...100.0,
+               label: {
+                    Text("Opacity")
+        },
+               minimumValueLabel: {
+                    Text("0")
+        },
+               maximumValueLabel: {
+                    Text("100")
+        })
+            
+            
+        
+        }
+        .padding()
     }
 }
 
